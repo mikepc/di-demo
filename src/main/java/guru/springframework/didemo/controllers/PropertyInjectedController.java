e850738bfec616a68c7f20c82a00e57bb1362f90
Michael Draper <mdraper@gmail.com>
2018-06-20 19:03:01 -0700

    headscratcher

---
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -0,0 +1,9 @@
+package guru.springframework.didemo.controllers;
+
+/**
+ * Author: Michael Draper
+ * Date: 6/20/18
+ **/
+
+public class PropertyInjectedController {
+}
---
+++ src/main/java/guru/springframework/didemo/services/GreetingService.java
@@ -0,0 +1,9 @@
+package guru.springframework.didemo.services;
+
+/**
+ * Author: Michael Draper
+ * Date: 6/20/18
+ **/
+
+public interface GreetingService {
+}
